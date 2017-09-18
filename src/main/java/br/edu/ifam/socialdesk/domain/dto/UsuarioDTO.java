d99c65a5921fa42a20f79b39a24ef3defcc4c199
Geison Santos <geison.sn@gmail.com>
2017-09-17 22:28:56 -0400

    Corrige o carregamento de chamados do usuário

--- src/main/java/br/edu/ifam/socialdesk/domain/dto/UsuarioDTO.java
+++ src/main/java/br/edu/ifam/socialdesk/domain/dto/UsuarioDTO.java
@@ -6,10 +6,19 @@ public class UsuarioDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	private Long id;
 	private String nome;
 	private String email;
 	private String senha;
 
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
